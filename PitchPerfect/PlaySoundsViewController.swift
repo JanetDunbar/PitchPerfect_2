65e08987e424fbffc4c1a5a8bb130f74d9dec0ff
jmd <jmd@amberlight.com>
2016-03-26 13:48:17 -0500

    Added and connected to PlaySoundsViewController the snailButton, chipmunkButton, rabbitButton, vaderButton, echoButton, reverbButton, stopButton IBOutlets.

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -11,14 +11,13 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
-    //save for later
-//    @IBOutlet weak var snailButton: UIButton!
-//    @IBOutlet weak var chipmunkButton: UIButton!
-//    @IBOutlet weak var rabbitButton: UIButton!
-//    @IBOutlet weak var vaderButton: UIButton!
-//    @IBOutlet weak var echoButton: UIButton!
-//    @IBOutlet weak var reverbButton: UIButton!
-//    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
     
     var recordedAudio: NSURL!
     
